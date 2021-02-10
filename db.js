ac4eb9ba2e899cf303881d31550465a8d78532f5
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-02-10 10:54:35 -0500

    update database config

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'andydo',
-    //password: '',
-    database: 'lovewav'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 module.exports = pool;
