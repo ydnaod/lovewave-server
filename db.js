259a88987b25b3aaa7dcdbe5b52cd4ba37c91953
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-03-12 15:10:26 -0500

    update database info with heroku credentials

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
