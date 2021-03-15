4b7d65d5dced70860e330b268d8da84451fd19af
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-03-15 00:28:52 -0400

    ensure the admin profile is not fetched

--- routes/fetchProfiles.js
+++ routes/fetchProfiles.js
@@ -9,7 +9,7 @@ router.use(authorization);
 router.get('/random', async (req, res) => {
     try {
         //console.log(req.user)
-        const query = await pool.query('select id from user_account where id not in (select other_user_account_id from swipes where user_account_id=$1) and id != $1 limit 10', [req.user]);
+        const query = await pool.query('select id from user_account where id not in (select other_user_account_id from swipes where user_account_id=$1) and id != $1 and id != 999 limit 10', [req.user]);
         res.json(query.rows);
     } catch (error) {
         console.error(error.message);
